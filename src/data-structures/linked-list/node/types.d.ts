141b4ef0678e93923714f12988819641640a0549
Nadno <eunadno@gmail.com>
2023-04-09 10:55:51 -0300

    fixup! improve: removeNext node from SNode
    improve: removeNext node from SNode

--- src/data-structures/linked-list/node/types.d.ts
+++ src/data-structures/linked-list/node/types.d.ts
@@ -2,5 +2,5 @@ export interface ISingleLinkedNode<T> {
   value: T;
   next: ISingleLinkedNode<T> | undefined;
   insertNext(value: T | ISingleLinkedNode<T>): void;
-  removeNext(): T | undefined;
+  removeNext(): ISingleLinkedNode<T> | undefined;
 }
